41621124c1c2d7559302cc7f324b6a009deed9d1
Daniil Solovev <solovevdaniil01@gmail.com>
2023-12-19 16:12:01 +0300

    First commit

---
+++ BiznesProfiApp/dbEntities/DBStorrage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiznesProfiApp.dbEntities
+{
+    public static class DBStorrage
+    {
+        public static BiznesProfiAppDBEntities DB_s { get; set; } = new BiznesProfiAppDBEntities();
+    }
+}
